719536a8d0dbeaff2eacd50aef7a052160d54d40
nOBITa <iampossible3001@gmail.com>
2015-06-06 21:34:25 +0700

    Commit - Initialize Domain project and Continent domain.

---
+++ Concept.Domain/Continent/IContinentRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Concept.Domain.Continent
+{
+	using Infrastructure.Common.Domain;
+	using System.Linq;
+	using System.Threading.Tasks;
+
+	public interface IContinentRepository : IRepository<Continent, string>
+	{
+		Task<Continent> GetAsync(string id);
+		IQueryable<Continent> GetAllQuery();
+	}
+}
