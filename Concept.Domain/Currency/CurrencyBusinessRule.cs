d58eb4fbf6ab75421fe6d69e7cb186b9421b9118
nOBITa <iampossible3001@gmail.com>
2015-06-07 23:12:36 +0700

    Commit - Initialize Country, Currency, and Language domains.

---
+++ Concept.Domain/Currency/CurrencyBusinessRule.cs
@@ -0,0 +1,37 @@
+﻿namespace Concept.Domain.Currency
+{
+	using Infrastructure.Common.Domain;
+
+	internal class CurrencyBusinessRule
+	{
+		/// <summary>
+		/// Business rule of currency ID required.
+		/// </summary>
+		public static readonly BusinessRule CurrencyIDRequired = new BusinessRule("A currency must has an ID");
+
+		/// <summary>
+		/// Business rule of currency name required.
+		/// </summary>
+		public static readonly BusinessRule CurrencyNameRequired = new BusinessRule("A currency must has a name");
+
+		/// <summary>
+		/// Business rule of currency sign required.
+		/// </summary>
+		public static readonly BusinessRule CurrencySignRequired = new BusinessRule("A currency must has a sign");
+
+		/// <summary>
+		/// Business rule of currency sign position required.
+		/// </summary>
+		public static readonly BusinessRule CurrencySignPositionRequired = new BusinessRule("A currency must has a sign position");
+
+		/// <summary>
+		/// Business rule of currency record created by required.
+		/// </summary>
+		public static readonly BusinessRule RecordCreatedByRequired = new BusinessRule("A currency must has a record created by");
+
+		/// <summary>
+		/// Business rule of currency record created when required.
+		/// </summary>
+		public static readonly BusinessRule RecordCreatedWhenRequired = new BusinessRule("A currency must has a record created when");
+	}
+}
---
+++ Concept.Domain/Language/Language.cs
@@ -0,0 +1,64 @@
+﻿namespace Concept.Domain.Language
+{
+	using Infrastructure.Common.Domain;
+	using System;
+
+	public class Language : EntityBase<string>, IAggregateRoot
+	{
+		#region Properties
+
+		/// <summary>
+		/// Gets or sets language name.
+		/// </summary>
+		public string Name { get; set; }
+
+		/// <summary>
+		/// Gets or sets language native name.
+		/// </summary>
+		public string NativeName { get; set; }
+
+		/// <summary>
+		/// Gets or sets language fall back.
+		/// </summary>
+		public string Fallback { get; set; }
+
+		#endregion
+
+		#region Implementations
+
+		public override void Validate()
+		{
+			if (string.IsNullOrWhiteSpace(this.ID))
+			{
+				this.AddBrokenRule(LanguageBusinessRule.LanguageIDRequired);
+			}
+
+			if (string.IsNullOrWhiteSpace(this.Name))
+			{
+				this.AddBrokenRule(LanguageBusinessRule.LanguageNameRequired);
+			}
+
+			if (string.IsNullOrWhiteSpace(this.NativeName))
+			{
+				this.AddBrokenRule(LanguageBusinessRule.LanguageNativeNameRequired);
+			}
+
+			if (string.IsNullOrWhiteSpace(this.Fallback))
+			{
+				this.AddBrokenRule(LanguageBusinessRule.LanguageFallbackRequired);
+			}
+
+			if (string.IsNullOrWhiteSpace(this.RecCreatedBy))
+			{
+				this.AddBrokenRule(LanguageBusinessRule.RecordCreatedByRequired);
+			}
+
+			if (this.RecCreatedWhen == DateTime.MinValue)
+			{
+				this.AddBrokenRule(LanguageBusinessRule.RecordCreatedWhenRequired);
+			}
+		}
+
+		#endregion
+	}
+}
