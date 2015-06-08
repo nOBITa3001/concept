8697b0083fd4b50b62f18c1cb7c0357df9cd6f8a
nOBITa <iampossible3001@gmail.com>
2015-06-09 01:37:44 +0700

    Commit - Initialize CountryLanguage domain.

--- Concept.Domain/Country/ICountryRepository.cs
+++ Concept.Domain/Country/ICountryRepository.cs
@@ -6,7 +6,20 @@
 
 	public interface ICountryRepository : IRepository<Country, string>
 	{
+		#region Country
+
 		Task<Country> GetAsync(string id);
 		IQueryable<Country> GetAllQuery();
+
+		#endregion
+
+		#region CountryLanguage
+
+		Task<CountryLanguage> GetCountryLanguageAsync(CountryLanguageID id);
+		IQueryable<CountryLanguage> GetAllCountryLanguageQuery();
+		void InsertCountryLanguage(CountryLanguage countryLanguage);
+		void DeleteCountryLanguage(CountryLanguage countryLanguage);
+
+		#endregion
 	}
 }
